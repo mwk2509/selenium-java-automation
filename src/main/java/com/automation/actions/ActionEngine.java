7d616632865ccd8eb945060c69f9f045fda5e9c8
mwk2509 <metasebiyakidane@gmail.com>
2023-11-13 14:38:20 -0500

    WindowsPage

--- src/main/java/com/automation/actions/ActionEngine.java
+++ src/main/java/com/automation/actions/ActionEngine.java
@@ -5,6 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
@@ -18,6 +19,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
@@ -34,6 +36,14 @@ public class ActionEngine extends BaseTest{
 		return we;
 	}
 	
+	public String getText(By locator) throws Exception {
+		String text = "";
+		WebElement we = getWebElement(locator);
+		text = we.getText();
+		return text;
+		
+	}
+	
 	public void click(By locator, String locatorName) throws Exception {
 		boolean flag = false;
 		try {
@@ -48,6 +58,7 @@ public class ActionEngine extends BaseTest{
 			e.printStackTrace();
 			flag = false;
 		} finally {
+			System.out.println("Success...");
 			if(flag) {
 				extentTest.log(LogStatus.PASS, "Succefully clicked on "+locatorName);
 			}else {
@@ -211,6 +222,61 @@ public class ActionEngine extends BaseTest{
 		
 		return screenShotLocation;
 		
+	}
+	
+	public void doubleClick(By locator, String locatorName) throws Exception{
+		boolean flag = false;
+		try {
+			
+			Actions actions = new Actions(driver);
+			actions.doubleClick().perform();
+			flag =true;
+		}catch(Exception e) {
+			e.printStackTrace();
+			flag =false;
+		}finally {
+			if (flag) {
+			} else {
+			}
+		}
+		}
+	public void drgAndDrop(By locator, String locatorName) throws Exception {
+		boolean flag = false;
+		try {
+			Actions act = new Actions(driver);
+			WebElement draggableObject = getWebElement(locator);
+			//WebElement draggableObject = driver.findElement(locator);
+			//act.dragAndDrop( locator, locatorName).perform();
+			flag =true;
+		}catch(Exception e) {
+			e.printStackTrace();
+			flag =false;
+		}finally {
+			if (flag) {
+			} else {
+		}
+	}
+	}
+	
+	public void switchToWindow(int windowIndex) throws Exception{
+		boolean flag = false;
+		try {
+			System.out.println("I'm in switchToWindow().......!");
+			ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+			System.out.println(tabs.size());
+			if(windowIndex < tabs.size()) {
+				driver.switchTo().window(tabs.get(windowIndex));
+			}
+			
+			flag =true;
+		}catch(Exception e) {
+			e.printStackTrace();
+			flag =false;
+		}finally {
+			if (flag) {
+			} else {
+			}
+		}
 	}
 	
 }
